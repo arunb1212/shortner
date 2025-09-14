343d88a71694f54e84632d30d94852f3628cf271
arunb1212 <arunbhagat302@gmail.com>
2025-09-14 21:09:23 +0530

    Add FAQ accordion component to Home page
    Introduced a reusable accordion UI component using @radix-ui/react-accordion and added an FAQ section to the Home page. Updated dependencies in package.json and package-lock.json to include @radix-ui/react-accordion.

---
+++ src/Components/Accrodian.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+
+const Accrodian = () => {
+  return (
+    <div className='w-2/3 mt-[30px]'>
+        <h1 className='text-2xl'>FAQS</h1>
+        <hr  className='w-full border border-white'/>
+        <Accordion type="multiple" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+  <AccordionItem value="item-2">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+  <AccordionItem value="item-3">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+  <AccordionItem value="item-4">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+    </div>
+  )
+}
+
+export default Accrodian
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import Accrodian from '@/Components/Accrodian'
 
 const Home = () => {
   return (
-    <div className='my-[10px] text-3xl font-bold text-center sm:text-4xl md:text-5xl lg:text-6xl p-[10px]'>
-    <h1>The only link shortner you'll ever needed</h1>
+    <div >
+    <h1 className='my-[10px] text-3xl font-bold text-center sm:text-4xl md:text-5xl lg:text-6xl p-[10px]'>The only link shortner you'll ever needed</h1>
+    <div className='w-full flex  flex-col  justify-center items-center'>
+        <form className='flex justify-center flex-col sm:flex-row items-center w-full px-[30px] gap-[10px] mb-[20px] ' >
+            <input type="url" placeholder='Enter your link' className='w-2/4 border rounded-md border-white outline-none p-[10px]' />
+            <Button type='submit'>Shorten</Button>
+        </form>
+        <div className='w-full px-[30px] flex justify-center items-center'>
+        <img src="https://github.com/piyush-eon/url-shortener/blob/master/public/banner1.jpg?raw=true" alt="" />
+        </div>
+
+<Accrodian/>
+    </div>
 </div>
   )
 }
