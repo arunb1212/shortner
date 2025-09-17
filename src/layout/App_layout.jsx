bc74201e833b63775e636c102e1fe1d7d5f60ec7
arunb1212 <arunbhagat302@gmail.com>
2025-09-17 22:16:27 +0530

    Update App_layout.jsx

--- src/layout/App_layout.jsx
+++ src/layout/App_layout.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '@/Components/Header'
-import Landing from '@/Components/Landing'
 const App_layout = () => {
   return (
     <div >
       <div className='min-h-screen'>
       <Header/>
-      {/* <Landing/> */}
       <Outlet/>
       </div>
 
