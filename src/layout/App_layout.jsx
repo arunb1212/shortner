ea8e1651e713d1f5c3de11dbeb688c932a8627bf
arunb1212 <arunbhagat302@gmail.com>
2025-09-17 22:13:24 +0530

    Fix import case sensitivity issue for Landing component

--- src/layout/App_layout.jsx
+++ src/layout/App_layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '@/Components/Header'
-import Landing from '@/Components/landing'
+import Landing from '@/Components/Landing'
 const App_layout = () => {
   return (
     <div >
